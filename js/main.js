46456e2c55c8a0e778f736ed07918968a1d8d114
Sandra Romero <romeroortiz.sandra@gmail.com>
2019-11-20 10:16:07 +0100

    Add conditional structure

--- js/main.js
+++ js/main.js
@@ -3,19 +3,27 @@ const button = document.querySelector(".js-button");
 const randomNumber = getRandomNumber(100);
 const numField = document.querySelector(".js-numberField");
 const clueField = document.querySelector(".js-clue");
-const number = numField.innerHTML.value;
+const number = numField.value;
+const clue = clueField.value;
 
 function getRandomNumber(max) {
   return Math.ceil(Math.random() * max);
 }
 
 console.log(randomNumber);
-console.log(number);
 
-// const checkNumberquality = function(introducedNumber){
-//      if (introducedNumber.value <1 && >100){
-//       clueField.innerHTML.value = "El número debe estar entre 0 y 100"
-//      }
-//    }
+const checkNumberquality = function() {
+  console.log(number);
 
-// button.addEventListener("click", );
+  if (number < 1 || number > 100) {
+    console.log("número fuera de rango");
+  } else if (number === randomNumber) {
+    //mensaje enhorabuena
+  } else if (number < randomNumber) {
+    // num muy bajo
+  } else {
+    // numero muy alto
+  }
+};
+
+button.addEventListener("click", checkNumberquality);
