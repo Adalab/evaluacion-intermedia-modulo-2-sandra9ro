67cd319cc83ef892fcdc1b95d7c639703b0bc783
Sandra Romero <romeroortiz.sandra@gmail.com>
2019-11-23 19:07:01 +0100

    finish interactivity and change styles

--- js/main.js
+++ js/main.js
@@ -1,29 +1,55 @@
 "use strict";
+// debugger;
 const button = document.querySelector(".js-button");
-const randomNumber = getRandomNumber(100);
 const numField = document.querySelector(".js-numberField");
 const clueField = document.querySelector(".js-clue");
-const number = numField.value;
-const clue = clueField.value;
+const shotField = document.querySelector(".js-shot");
+
+//random number
+
+const randomNumber = getRandomNumber(100);
 
 function getRandomNumber(max) {
   return Math.ceil(Math.random() * max);
 }
 
-console.log(randomNumber);
+console.log("El número aleatorio es:" + randomNumber);
 
-const checkNumberquality = function() {
-  console.log(number);
+//Read number written by user with random number
 
-  if (number < 1 || number > 100) {
-    console.log("número fuera de rango");
-  } else if (number === randomNumber) {
-    //mensaje enhorabuena
-  } else if (number < randomNumber) {
-    // num muy bajo
-  } else {
-    // numero muy alto
+const compareNumber = function() {
+  const numberFieldValue = parseInt(numField.value);
+  if (numberFieldValue < 1 || numberFieldValue > 100) {
+    clueFieldValue('Debes introducir un número entre 1 y 100');
+  } else if (numberFieldValue === randomNumber) {
+    clueFieldValue('¡¡¡Enhorabuena, has acertado el número secreto!!!');
+  } else if (numberFieldValue < randomNumber) {
+    clueFieldValue('Tu número es muy bajo');
+  } else if (numberFieldValue > randomNumber) {
+    clueFieldValue('Tu número es muy alto');
+  // } else {
+  //   clueFieldValue('Debes introducir un número entre 1 y 100');
   }
 };
 
-button.addEventListener("click", checkNumberquality);
+function clueFieldValue(text) {
+  clueField.value = text;
+}
+
+// Counting number of shots
+
+let tries = 0;
+
+const countShotsNumber = function(){
+  tries = tries + 1; 
+  shotField.value = 'Número de intentos: ' + tries;
+}
+
+
+// Listening to button
+ 
+const handleButton = function() {
+ compareNumber();
+ countShotsNumber();
+};
+button.addEventListener("click", handleButton);
